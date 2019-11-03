ff60e5a0c429841d25067bfb594bf7d44bd09b15
AlexCCo <55455549+AlexCCo@users.noreply.github.com>
2019-11-03 18:40:40 +0100

    Add files via upload

---
+++ simple_kernel/AlexCCoWork/include/peripherals/base.h
@@ -0,0 +1,9 @@
+#ifndef	_P_BASE_H
+#define	_P_BASE_H
+
+//We will be using this memoru base value because we'll be working directly
+//to the ARM peripherals (see section 1.2.3 from BCM2837 peripherals
+//documentation for more info)
+#define PBASE 0x3F000000
+
+#endif  /*_P_BASE_H */
