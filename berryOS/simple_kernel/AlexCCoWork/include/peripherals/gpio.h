222f8eb377318ef814efce05b6b948ad937ac193
AlexCCo <canceloalejandro@gmail.com>
2019-11-04 02:18:50 +0100

    Attempt to merge the led functionality on AlexCCo work

--- berryOS/simple_kernel/AlexCCoWork/include/peripherals/gpio.h
+++ berryOS/simple_kernel/AlexCCoWork/include/peripherals/gpio.h
@@ -5,7 +5,7 @@
 //all defined in page 89 and forward
 
 //GPIO function select (registers 0 to 9)
-#define GPFSEL1         (PBASE+0x00200000)
+#define GPFSEL0         (PBASE+0x00200000)
 //GPIO function select (registers 10 to 19)
 #define GPFSEL1         (PBASE+0x00200004) 
 //GPIO function select (registers 20 to 29)
@@ -17,38 +17,60 @@
 //GPIO function select (registers 50 to 53)
 #define GPFSEL5         (PBASE+0x00200014) 
 
+//GPIO pin output set (GPIO pin 0-31)
+#define GPSET0          (PBASE+0x0020001C) 
+//GPIO pin output set (GPIOpin 32-53)
+#define GPSET1          (PBASE+0x00200020) 
 
-#define GPSET0          (PBASE+0x0020001C) //GPIO pin output set (GPIO pin 0-31)
-#define GPSET1          (PBASE+0x00200020) //GPIO pin output set (GPIOpin 32-53)
+//GPIO pin output clear (GPIO pin 0-31)
+#define GPCLR0          (PBASE+0x00200028) 
+//GPIO pin output clear (GPIOpin 32-53)
+#define GPCLR1          (PBASE+0x0020002C) 
 
-#define GPCLR0          (PBASE+0x00200028) //GPIO pin output clear (same as set)
-#define GPCLR1          (PBASE+0x0020002C) //GPIO pin output clear (same as set)
-
-#define GPLEV0          (PBASE+0x00200034) //GPIO pin level
+//GPIO pin level (GPIO pin 0-31)
+#define GPLEV0          (PBASE+0x00200034) 
+//GPIO pin level (GPIO pin 32-53)
 #define GPLEV1          (PBASE+0x00200038) 
 
-#define GPEDS0          (PBASE+0x00200040) //GPIO pin event detect status 0
+//GPIO pin event detect status 0 (GPIO pin 0-31)
+#define GPEDS0          (PBASE+0x00200040) 
+//GPIO pin event detect status 0 (GPIO pin 32-53)
 #define GPEDS1          (PBASE+0x00200044) 
 
-#define GPREN0          (PBASE+0x0020004C) //GPIO rising-edge pin detect enable
+//GPIO rising-edge pin detect enable (GPIO pin 0-31)
+#define GPREN0          (PBASE+0x0020004C) 
+//GPIO rising-edge pin detect enable (GPIO pin 32-53)
 #define GPREN1          (PBASE+0x00200050)
 
-#define GPFEN0          (PBASE+0x00200058) //GPIO falling-edge pin detect enable
+//GPIO falling-edge pin detect enable (GPIO pin 0-31)
+#define GPFEN0          (PBASE+0x00200058) 
+//GPIO falling-edge pin detect enable (GPIO pin 32-53)
 #define GPFEN1          (PBASE+0x0020005C)
 
-#define GPHEN0          (PBASE+0x02000064) //GPIO pin high detect enable
+//GPIO pin high detect enable (GPIO pin 0-31)
+#define GPHEN0          (PBASE+0x02000064) 
+//GPIO pin high detect enable (GPIO pin 32-53)
 #define GPHEN1          (PBASE+0x02000068)
 
-#define GPLEN0          (PBASE+0x02000070) //GPIO pin low detect enable
+//GPIO pin low detect enable (GPIO pin 0-31)
+#define GPLEN0          (PBASE+0x02000070) 
+//GPIO pin low detect enable (GPIO pin 32-53)
 #define GPLEN1          (PBASE+0x02000074)
 
-#define GPAREN0         (PBASE+0x0200007C) //GPIO Pin Async. rising edge detect 
+//GPIO Pin Async. rising edge detect (GPIO pin 0-31)
+#define GPAREN0         (PBASE+0x0200007C) 
+//GPIO Pin Async. rising edge detect (GPIO pin 32-53)
 #define GPAREN1         (PBASE+0x02000080)
 
-#define GPALEN0         (PBASE+0x02000088) //GPIO Pin Async.falling edge detect 
+//GPIO Pin Async.falling edge detect (GPIO pin 0-31)
+#define GPALEN0         (PBASE+0x02000088) 
+//GPIO Pin Async.falling edge detect (GPIO pin 32-53)
 #define GPALEN1         (PBASE+0x0200008C)
 
-#define GPPUD           (PBASE+0x00200094) //GPIO pin pull-up/down register
-#define GPPUDCLK0       (PBASE+0x00200098) //GPIO pin pull-up/down enable clock
+//GPIO pin pull-up/down register
+#define GPPUD           (PBASE+0x00200094) 
+//GPIO pin pull-up/down enable clock (GPIO pin 0-31)
+#define GPPUDCLK0       (PBASE+0x00200098) 
+//GPIO pin pull-up/down enable clock (GPIO pin 32-53)
 #define GPPUDCLK1       (PBASE+0x0020009C) 
 #endif  /*_P_GPIO_H */
--- berryOS/simple_kernel/AlexCCoWork/include/utils.h
+++ berryOS/simple_kernel/AlexCCoWork/include/utils.h
@@ -1,8 +1,8 @@
 #ifndef	_BOOT_H
 #define	_BOOT_H
 
-extern void delay ( unsigned long);
-extern void put32 ( unsigned long, unsigned int );
-extern unsigned int get32 ( unsigned long );
+extern void delay ( unsigned long); //parameters: (address)
+extern void put32 ( unsigned long, unsigned int ); //parameters: (address, value)
+extern unsigned int get32 ( unsigned long ); //parameters: (address)
 
 #endif  /*_BOOT_H */
