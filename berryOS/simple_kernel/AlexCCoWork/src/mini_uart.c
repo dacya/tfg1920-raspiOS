f68e8824e2ce10e3609402d89d7c1d2a395d3289
AlexCCo <canceloalejandro@gmail.com>
2019-11-04 23:09:33 +0100

    Nothing

--- berryOS/simple_kernel/AlexCCoWork/src/mini_uart.c
+++ berryOS/simple_kernel/AlexCCoWork/src/mini_uart.c
@@ -4,21 +4,14 @@
 
 void uart_send ( char c )
 {
-	delay(150);
-	while(1) {
-		if(get32(AUX_MU_LSR_REG)&0x20) 
-			break;
-	}
+	//we have to do a wait here
+
 	put32(AUX_MU_IO_REG,c);
 }
 
 char uart_recv ( void )
 {
-	delay(150);
-	while(1) {
-		if(get32(AUX_MU_LSR_REG)&0x01) 
-			break;
-	}
+	//we have to do a wait here
 	return(get32(AUX_MU_IO_REG)&0xFF);
 }
 
@@ -31,21 +24,30 @@ void uart_send_string(char* str)
 
 void uart_init ( void )
 {
-	unsigned int selector;
+	unsigned int selector = 0;
 
 	selector = get32(GPFSEL1);
-	selector &= ~(7<<12);                   // clean gpio14
-	selector |= 2<<12;                      // set alt5 for gpio14
-	selector &= ~(7<<15);                   // clean gpio15
-	selector |= 2<<15;                      // set alt5 for gpio15
+	//selector &= ~(7<<12);                   // clean gpio14
+	selector |= (2<<12);                      // set alt5 for gpio14
+	//selector &= ~(7<<15);                   // clean gpio15
+	selector |= (2<<15);                      // set alt5 for gpio15
 	put32(GPFSEL1,selector);
 
 	put32(GPPUD,0);
 	delay(150);
-	put32(GPPUDCLK0,(1<<14)|(1<<15));
+	put32(GPPUDCLK0,((1<<14)|(1<<15)));
+	put32(GPPUDCLK1,0);
 	delay(150);
+	put32(GPPUD,0);
 	put32(GPPUDCLK0,0);
+	put32(GPPUDCLK1,0);
 
+	/*maybe i should be using AUX_MU_IIR_REG register?
+		on read just set bit 2:1 to 00
+		on write, writing with bit 1 set, it clears the receive FIFO. with bit
+		2 set, it clears the transmit FIFO
+		the rest leave it as it is.
+	*/
 	put32(AUX_ENABLES,1);                   //Enable mini uart (this also enables access to it registers)
 	put32(AUX_MU_CNTL_REG,0);               //Disable auto flow control and disable receiver and transmitter (for now)
 	put32(AUX_MU_IER_REG,0);                //Disable receive and transmit interrupts
