eb9539d8043a6240440214251e70132a839b4aa2
AlexCCo <canceloalejandro@gmail.com>
2019-11-04 21:15:55 +0100

    Translated file utils.S to ARMv7 architecture

--- berryOS/simple_kernel/AlexCCoWork/include/utils.h
+++ berryOS/simple_kernel/AlexCCoWork/include/utils.h
@@ -1,8 +1,20 @@
 #ifndef	_BOOT_H
 #define	_BOOT_H
 
-extern void delay ( unsigned long); //parameters: (address)
-extern void put32 ( unsigned long, unsigned int ); //parameters: (address, value)
-extern unsigned int get32 ( unsigned long ); //parameters: (address)
+/*
+    parameters: (num cycles)
+    This function takes a numer of cycles to be stopped
+*/
+extern void delay ( unsigned long); 
+/*
+    parameters: (address, value)
+    This function puts 32 bits into the specified address
+*/
+extern void put32 ( unsigned long, unsigned int ); 
+/*
+    parameters: (address)
+    This function obtains the 32 bits from an address
+*/
+extern unsigned int get32 ( unsigned long ); 
 
 #endif  /*_BOOT_H */
