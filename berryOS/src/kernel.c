0b92b0e752e59c15296742adc01c52b78cbcc07f
AlexCCo <canceloalejandro@gmail.com>
2020-02-25 20:43:04 +0100

    Leaving interrupts in a good state

--- berryOS/src/kernel.c
+++ berryOS/src/kernel.c
@@ -17,10 +17,22 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
     pin_set_function(17, OUTPUT);
     
     /* UART */
-    //uart_init();
-    //uart_puts("Hello, World!\r\n\r\n");
+    uart_init();
+    uart_puts("Hello, World!\r\n\r\n");
 
+    /* INTERRUPTS */
+    uart_puts(">> Local timer init: ");
+    local_timer_init();
+    uart_puts(" [OK] \r\n");
     
+    uart_puts(">> Interrupts init: ");
+    interrupts_init();
+    uart_puts(" [OK] \r\n");
+    
+    uart_puts(">> Register timer handler and clearer: ");
+    register_irq_handler(ARM_TIMER, local_timer_handler, local_timer_clearer);
+    uart_puts(" [OK] \r\n");
+
     /* DYNAMIC MEMORY */
     //uart_puts(">> Dynamic memory: ");
     mem_init(((atag_t *)atags));
